a810305f1460257d88b2578279e510888a1150ec
Kacper Siuda <kacper.siuda@hotmail.com>
2016-04-21 18:57:44 +0200

    zmiany menu

--- HtmlProjects/Learning/js/myScript.js
+++ HtmlProjects/Learning/js/myScript.js
@@ -2,11 +2,14 @@ $(document).ready(toggle(),addPost());
 
 function toggle() {
 	var hide = true;
+	var $footer = $('#footer'); 
+	var position = $footer.offset();
     $('.glyphicon-align-justify').on('click', function (event) {
 		if (hide) {
 			$('#menu').addClass("menu-slide-show");
 			$(this).css("color", "deeppink");
 			$(this).css("font-size", "3em");
+			$('#menu').css("height",position.top - 130);
 			hide = false;
 		} else {
 			$('#menu').removeClass("menu-slide-show");
@@ -19,7 +22,10 @@ function toggle() {
 function addPost() {
 	
 	$('.glyphicon-plus-sign').on('click', function (event) {
-		alert("test");
+		var $footer = $('.footer'); 
+		var position = $footer.position();
+		
+		alert(position.top);
 
 	});
 }
