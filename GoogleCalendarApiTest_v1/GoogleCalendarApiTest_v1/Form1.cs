31a6de7675f5c170a027eaae5ecab320d337d3b2
KacperSiuda <kacper.siuda@hotmail.com>
2016-05-05 00:48:26 +0200

    test

---
+++ GoogleCalendarApiTest_v1/GoogleCalendarApiTest_v1/Form1.cs
@@ -0,0 +1,41 @@
+﻿using GoogleCalendarApiTest_v1.GoogleAPI;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GoogleCalendarApiTest_v1
+{
+    public partial class Form1 : Form
+    {
+        private GoogleApiConector calendarConect = new GoogleApiConector();
+        public Form1()
+        {
+            InitializeComponent();
+            conectToCalendar();
+        }
+        public void conectToCalendar()
+        {
+            try
+            {
+                calendarConect.CreateCredential();
+
+            }
+            catch (Exception e)
+            {
+
+                MessageBox.Show(e.ToString());
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            calendarConect.CreateEvent(textBox1.Text, dateTimePicker1.Value, dateTimePicker2.Value);
+        }
+    }
+}
---
+++ GoogleCalendarApiTest_v1/GoogleCalendarApiTest_v1/GoogleAPI/GoogleApiConector.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Google.Apis.Auth.OAuth2;
+using System.Threading;
+using Google.Apis.Util.Store;
+using Google.Apis.Calendar.v3;
+using Google.Apis.Services;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+using Google.Apis.Calendar.v3.Data;
+
+namespace GoogleCalendarApiTest_v1.GoogleAPI
+{
+    public class GoogleApiConector
+    {
+        private static string[] Scopes = { CalendarService.Scope.Calendar };
+        private static string ApplicationName = "Google Calendar API .NET Quickstart";
+        private UserCredential credential;
+
+        public void CreateCredential()
+        {
+            using (var stream = new FileStream("client_secret.json", FileMode.Open, FileAccess.Read))
+            {
+                string credPath = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+                credPath = Path.Combine(credPath, ".credentials/calendar.json");
+
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    Scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+
+            }
+        }
+        public void CreateEvent(string summary, DateTime start, DateTime end)
+        {
+            var service = new CalendarService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });
+            Event newEvent = new Event()
+            {
+                Summary = summary,
+                Start = new EventDateTime()
+                {
+                    DateTime = DateTime.Parse(start.ToString())
+                },
+                End = new EventDateTime()
+                {
+                    DateTime = DateTime.Parse(end.ToString())
+                }
+
+            };
+            String calendarId = "primary";
+            EventsResource.InsertRequest request = service.Events.Insert(newEvent, calendarId);
+            Event createdEvent = request.Execute();
+
+        }
+
+    }
+}
